f4df408a172eb9b69361c5b63172aa9b4e75bcc2
Qasura75 <112414285+Qasura75@users.noreply.github.com>
2023-08-13 19:37:00 +0100

    Added Operating function

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+let numA;
+let numB;
+let operator;
+
+function operate() {
+    numA = parseInt(prompt("First Number"))
+    operator = prompt("Operator");
+    numB = parseInt(prompt("Second Number"));
+
+    switch (operator) {
+        case "+":
+            add(numA, numB)
+            break;
+        case "-":
+            subtract(numA, numB)
+            break;
+        case "*":
+            multiply(numA, numB)
+            break;
+        case "/":
+            divide(numA, numB)
+            break;
+    }
+    console.log(c);
+}
+
+operate();
+
 function add(a, b) {
     c = a+b;
     return c;
