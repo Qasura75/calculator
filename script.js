c0493deb4e52e5cc96250ed37e0e0aae49b8bddd
Qasura75 <112414285+Qasura75@users.noreply.github.com>
2023-08-13 19:24:09 +0100

    Added mathematical functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    c = a+b;
+    return c;
+}
+
+function subtract(a, b) {
+    c = a-b;
+    return c;
+}
+
+function multiply(a, b) {
+    c = a*b;
+    return c;
+}
+
+function divide(a, b) {
+    c = a/b;
+    return c;
+}
